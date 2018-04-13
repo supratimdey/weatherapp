01939b377595de0c7c96d0ce84746ff08f5f265d
Supratim <supratimdey@outlook.com>
2018-04-13 17:06:07 +0530

    just updated

--- weathertest.js
+++ weathertest.js
@@ -1,14 +1,16 @@
 const request = require('request');
+const argv = require('yargs').argv;
+
 let apiKey = '0e32fc352ee82286d41f814b44e4dfe9'
-let city = 'Hyderabad'
-let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+let city = argv.c ||'Hyderabad'
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 request(url,function(err,response, body){
   if (err) {
     console.log('Error :', err);
   } else {
     let weather = JSON.parse(body);
-    let message = `It's ${weather.main.temp} degree at ${weather.name},${weather.sys.country}`
+    let message = `It's ${weather.main.temp} degree F at ${weather.name},${weather.sys.country}`
     console.log(message);
   }
 });
